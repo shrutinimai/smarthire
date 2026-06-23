bac5f0efcc465a7448f620942c2a07fa7bd15eed
shrutinimai <nimaishruti@gmail.com>
2026-06-23 14:05:33 +0530

    Update mysqlDB.js

--- backend/config/mysqlDB.js
+++ backend/config/mysqlDB.js
@@ -7,15 +7,21 @@ const sequelize = new Sequelize(
   process.env.MYSQL_PASSWORD,
   {
     host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
     dialect: "mysql",
     logging: false,
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
   }
 );
 
 const connectMySQL = async () => {
   try {
     await sequelize.authenticate();
-    console.log(" MySQL connected successfully");
+    console.log("MySQL connected successfully");
     await sequelize.sync({ alter: true });
     console.log(" MySQL tables synced");
   } catch (error) {
