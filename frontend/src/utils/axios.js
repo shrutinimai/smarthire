a3b87f37a20a50d7f9d2bc817746bd73c1bbe3f7
shrutinimai <nimaishruti@gmail.com>
2026-06-25 23:47:55 +0530

    Update axios.js

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api",
+baseURL: "https://smarthire-backend-chzw.onrender.com/api",
 });
 
 instance.interceptors.request.use((config) => {
